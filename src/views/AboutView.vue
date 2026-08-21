f7d82d419e1761d2a17440b44b909eef89092c4b
Mishel Sigalov <mishelsigalov10@gmail.com>
2026-08-21 15:29:57 +0300

    Add fundamentals

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-card>
+      <v-card-title>About</v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Name">Username</v-text-field>
+          <v-text-field label="Email"></v-text-field>
+          <v-textarea label="Message"></v-textarea>
+          <v-btn color="primary">Submit</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
